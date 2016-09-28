790f56767f9ac5b01ba888cbec6fcb0b10ac13e1
Steve Northup <steve.northup@gmail.com>
2016-09-28 18:59:39 -0400

    Added database creation script

---
+++ create_database.sql
@@ -0,0 +1,18 @@
+
+CREATE DATABASE wedding_rsvp;
+
+CREATE USER 'app'@'localhost' IDENTIFIED BY 'b4RV;JpLqs7DSt(m';
+
+GRANT INSERT ON wedding_rsvp.* TO 'app'@'localhost';
+GRANT SELECT ON wedding_rsvp.* TO 'app'@'localhost';
+
+FLUSH PRIVILEGES;
+
+CREATE TABLE wedding_rsvp.guest (
+  id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  guestName VARCHAR(255),
+  coming TINYINT(1),
+  overnight TINYINT(1),
+  comments VARCHAR(255),
+  regDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
