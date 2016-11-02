fea4ce1fcb0d0c79b73d76f089e9e3a8efc5416a
Steve Northup <steve.northup@gmail.com>
2016-11-02 18:12:46 -0400

    Fixing RSVP and nav menu selection

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -10,6 +10,18 @@ $(window).scroll(function() {
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
+        $('.navbar-toggle').click();
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scrollhack').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
